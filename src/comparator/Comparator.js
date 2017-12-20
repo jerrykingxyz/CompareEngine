a7e58d4657fda489261529671ff8afd9ed5a0eb7
jerry <1332411592@qq.com>
2017-12-20 20:23:50 +0800

    doc : Comparator and Storage remarks

--- src/comparator/Comparator.js
+++ src/comparator/Comparator.js
@@ -1,8 +1,33 @@
+/**
+ * An abstract comparator class
+ * @class
+ */
 class Comparator {
+
+    /**
+     * preprocess the input data
+     * @param {*} obj Engine input arguments
+     * @return {*}
+     */
     preprocess (obj) {
         return obj;
     }
+
+    /**
+     * compare two data
+     * @param key1
+     * @param value1
+     * @param key2
+     * @param value2
+     * @return {boolean} Need update value2
+     */
     compare (key1, value1, key2, value2) {}
+
+    /**
+     * post-process the compare result before being stored
+     * @param key
+     * @param value
+     */
     postprocess (key, value) {}
 }
 
--- src/storage/Storage.js
+++ src/storage/Storage.js
@@ -1,7 +1,32 @@
+/**
+ * An abstract key-value storage class
+ * @class
+ */
 class Storage {
+
+    /**
+     * get key name
+     * @return {string}
+     */
     getKeyName() {}
+
+    /**
+     * get all the items to be compared
+     * @return {array, object} The format can be [keys], [{keyName}], [keys, {keyName}], {key: value}
+     */
     getAll () {}
+
+    /**
+     * @param {string} key
+     * @return {*}
+     */
     getValue (key) {}
+
+    /**
+     * @param {string} key If the key is null, you need to insert the value and return the key
+     * @param {object} value
+     * @return {string}
+     */
     setValue (key, value) {}
 }
 
